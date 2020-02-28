cb0975c6ad146f744325b0ca4a14f3edbbe453f7
Denis <skarrok.h@gmail.com>
2020-02-29 01:33:38 +0300

    simple install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ln -vrfs vimrc ~/.vimrc
+ln -vrfs gvimrc ~/.gvimrc
+ln -vrfs tmux.conf ~/.tmux.conf
+ln -vrfs zshrc ~/.zshrc
+ln -vrfs ginit.vim ~/.config/nvim/ginit.vim
+ln -vrfs init.vim ~/.config/nvim/init.vim
