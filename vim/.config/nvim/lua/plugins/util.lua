e901873e1a63fb65a2835bc6d7a0f75b421db984
skarrok <skarrok.h@gmail.com>
2025-02-25 19:32:22 +0300

    neovim: update plugins

--- vim/.config/nvim/lua/plugins/util.lua
+++ vim/.config/nvim/lua/plugins/util.lua
@@ -18,6 +18,7 @@ return {
     -- stylua: ignore
     keys = {
       { "<leader>qs", function() require("persistence").load() end,                desc = "Restore Session" },
+      { "<leader>qS", function() require("persistence").select() end,              desc = "Select Session" },
       { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
       { "<leader>qd", function() require("persistence").stop() end,                desc = "Don't Save Current Session" },
     },
